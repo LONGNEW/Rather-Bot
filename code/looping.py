c5da7d5a89135e7cf75f73718091bb3b2434159e
ShinHyoung <jsin2475@naver.com>
2022-02-10 16:51:31 +0900

    [Modify] loop time and scope of fields

--- code/looping.py
+++ code/looping.py
@@ -1,12 +1,9 @@
 import discord, datetime, parsing as tool, pytz
 from discord.ext import tasks, commands
 
-channels = dict()
-where = ["백마 광장", "학사 공지", "일반 소식", "사업단 소식"]
-KST = pytz.timezone("Asia/Seoul")
-
 class MyCog(commands.Cog):
     def __init__(self):
+        self.channels = dict()
         self.info = [dict() for _ in range(4)]
         self.prev_date = "22.02.09"
         self.notice.start()
@@ -14,9 +11,11 @@ class MyCog(commands.Cog):
     def notice_stop(self):
         self.notice.cancel()
 
-    @tasks.loop(minutes=1)
+    @tasks.loop(minutes=3)
     async def notice(self):
         print("keep crawl")
+        where = ["백마 광장", "학사 공지", "일반 소식", "사업단 소식"]
+        KST = pytz.timezone("Asia/Seoul")
         date = str(datetime.datetime.now(KST).date()).replace("-", ".")[2:]
 
         if date != self.prev_date:
@@ -28,7 +27,6 @@ class MyCog(commands.Cog):
             ret, cnt = tool.what_you_want(i, date), 0
             temp = discord.Embed(title=where[i], description=ret[0], color=0x62c1cc)
 
-            print(ret)
             for j in range(1, len(ret)):
                 title = ret[j][1]
                 if title in self.info[i]:
@@ -40,6 +38,7 @@ class MyCog(commands.Cog):
                 temp.add_field(name=title, value=ret[j][-1], inline=False)
 
             if cnt:
-                for ch in channels:
+                for ch in self.channels:
+                    print(f"{i}번쨰 크롤링 , send to : {ch.guild, ch.id}")
                     await ch.send("", embed=temp)
-        print()
+                print()
