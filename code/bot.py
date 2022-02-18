e228066f0c51f4b748b6f710b9dd35063ac3eaf7
ShinHyoung <jsin2475@naver.com>
2022-02-18 15:40:51 +0900

    modify : "정지" command reaction has changed

--- code/bot.py
+++ code/bot.py
@@ -3,7 +3,6 @@ import discord, looping, os
 class MyClient(discord.Client):
     async def on_ready(self):
         self.task = looping.MyCog()
-        self.work = 1
         await client.change_presence(activity=discord.Game("근무"))
         print('Logged on as', self.user)
 
@@ -21,9 +20,6 @@ class MyClient(discord.Client):
                 self.task.channels[message.channel] = 1
 
             # 근무를 하지 않고 있던 경우에는 재시작을 해서 메시지를 보내도록 해야함.
-            if not self.work:
-                self.work = 1
-                await self.task.notice.start()
 
             print(f"{message.guild.name}에서 [공지]를 입력")
             for ch in self.task.channels.keys():
@@ -34,16 +30,15 @@ class MyClient(discord.Client):
             await message.channel.send(f"채널 [{message.guild.name}]이 추가되었습니다.")
 
         if message.content == '정지':
-            await client.change_presence(activity=discord.Game("휴식"))
+            del self.task.channels[message.channel]
 
-            # 일을 하고 있던 상황에서는 bool 값을 변경해야 함.
-            if self.work:
-                self.work = 0
-                self.task.notice_stop()
+            print(f"{message.guild.name}에서 [정지]를 입력")
+            for ch in self.task.channels.keys():
+                print(ch.id, ch)
+            print()
 
-                print(f"{message.guild.name}에서 [정지]를 입력\n")
-                for ch in self.task.channels:
-                    await ch.send(f"{message.guild.name}에서 [정지]를 입력하였습니다.")
+            await message.channel.send(f"채널 [{message.guild.name}]의 연결이 제거되었습니다.")
+
 
 TOKEN = os.environ.get('BOT_TOKEN')
 client = MyClient()
