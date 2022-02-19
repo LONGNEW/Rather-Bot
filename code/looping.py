de76db0a40bbb176cc939e49b3ae24d9dbbf48b2
ShinHyoung <jsin2475@naver.com>
2022-02-20 02:18:06 +0900

    Update looping.py

--- code/looping.py
+++ code/looping.py
@@ -9,7 +9,7 @@ class MyCog(commands.Cog):
         self.channels = dict()
         self.info = [dict() for _ in range(4)]
         self.prev_date = "22.02.09"
-        self.crawl()
+        await self.crawl()
 
     @tasks.loop(minutes=2)
     async def crawl(self):
